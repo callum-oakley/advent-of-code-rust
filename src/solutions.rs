802caf20af9c74690e846eff08ca429ffe531c8b
Callum Oakley <hello@callumoakley.net>
2023-04-11 22:56:31 +0100

    2015 12

--- src/solutions.rs
+++ src/solutions.rs
@@ -12,6 +12,7 @@ mod year2015 {
     pub mod day09;
     pub mod day10;
     pub mod day11;
+    pub mod day12;
 }
 
 use std::collections::BTreeMap;
@@ -124,6 +125,15 @@ pub fn build() -> BTreeMap<u16, BTreeMap<u8, Solution>> {
             part1: Some(|input| year2015::day11::part1(input).to_string()),
             part2: Some(|input| year2015::day11::part2(input).to_string()),
             tests: Some(year2015::day11::tests),
+        },
+    );
+
+    solutions.entry(2015).or_insert_with(BTreeMap::new).insert(
+        12,
+        Solution {
+            part1: Some(|input| year2015::day12::part1(input).to_string()),
+            part2: Some(|input| year2015::day12::part2(input).to_string()),
+            tests: Some(year2015::day12::tests),
         },
     );
 
---
+++ src/solutions/year2015/day12.rs
@@ -0,0 +1,38 @@
+use serde_json::Value;
+
+fn sum_numbers(ignore_red: bool, v: &Value) -> i64 {
+    match v {
+        Value::Number(n) => n.as_i64().unwrap(),
+        Value::Array(a) => a.iter().map(|v| sum_numbers(ignore_red, v)).sum(),
+        Value::Object(m) if !ignore_red || m.values().all(|v| v != "red") => {
+            m.values().map(|v| sum_numbers(ignore_red, v)).sum()
+        }
+        _ => 0,
+    }
+}
+
+pub fn part1(input: &str) -> i64 {
+    let v: Value = serde_json::from_str(input).unwrap();
+    sum_numbers(false, &v)
+}
+
+pub fn part2(input: &str) -> i64 {
+    let v: Value = serde_json::from_str(input).unwrap();
+    sum_numbers(true, &v)
+}
+
+pub fn tests() {
+    assert_eq!(part1(r#"[1,2,3]"#), 6);
+    assert_eq!(part1(r#"{"a":2,"b":4}"#), 6);
+    assert_eq!(part1(r#"[[[3]]]"#), 3);
+    assert_eq!(part1(r#"{"a":{"b":4},"c":-1}"#), 3);
+    assert_eq!(part1(r#"{"a":[-1,1]}"#), 0);
+    assert_eq!(part1(r#"[-1,{"a":1}]"#), 0);
+    assert_eq!(part1(r#"[]"#), 0);
+    assert_eq!(part1(r#"{}"#), 0);
+
+    assert_eq!(part2(r#"[1,2,3]"#), 6);
+    assert_eq!(part2(r#"[1,{"c":"red","b":2},3]"#), 4);
+    assert_eq!(part2(r#"{"d":"red","e":[1,2,3,4],"f":5}"#), 0);
+    assert_eq!(part2(r#"[1,"red",5]"#), 6);
+}
