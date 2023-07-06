c060d7222de3e1f4da180d376bb23c5499dad949
Callum Oakley <hello@callumoakley.net>
2023-07-06 21:29:57 +0100

    2017 8

--- src/solutions.rs
+++ src/solutions.rs
@@ -64,6 +64,7 @@ mod year2017 {
     pub mod day05;
     pub mod day06;
     pub mod day07;
+    pub mod day08;
 }
 
 use std::collections::BTreeMap;
@@ -591,6 +592,15 @@ pub fn build() -> BTreeMap<u16, BTreeMap<u8, Solution>> {
             part1: Some(|input| year2017::day07::part1(input).to_string()),
             part2: Some(|input| year2017::day07::part2(input).to_string()),
             tests: Some(year2017::day07::tests),
+        },
+    );
+
+    solutions.entry(2017).or_insert_with(BTreeMap::new).insert(
+        8,
+        Solution {
+            part1: Some(|input| year2017::day08::part1(input).to_string()),
+            part2: Some(|input| year2017::day08::part2(input).to_string()),
+            tests: Some(year2017::day08::tests),
         },
     );
 
---
+++ src/solutions/year2017/day08.rs
@@ -0,0 +1,71 @@
+use std::{cmp, collections::HashMap};
+
+struct Instruction<'a> {
+    reg: &'a str,
+    op: &'a str,
+    diff: i32,
+    cond_lhs: &'a str,
+    cond_op: &'a str,
+    cond_rhs: i32,
+}
+
+fn parse(input: &str) -> impl Iterator<Item = Instruction> + '_ {
+    input.lines().map(|line| {
+        let mut words = line.split_whitespace();
+        Instruction {
+            reg: words.next().unwrap(),
+            op: words.next().unwrap(),
+            diff: words.next().unwrap().parse().unwrap(),
+            cond_lhs: words.nth(1).unwrap(),
+            cond_op: words.next().unwrap(),
+            cond_rhs: words.next().unwrap().parse().unwrap(),
+        }
+    })
+}
+
+fn part_(part: u8, input: &str) -> i32 {
+    let mut regs = HashMap::new();
+    let mut max_val = 0;
+    for instruction in parse(input) {
+        let lhs = regs.get(instruction.cond_lhs).unwrap_or(&0);
+        let op = match instruction.cond_op {
+            ">" => i32::gt,
+            "<" => i32::lt,
+            ">=" => i32::ge,
+            "<=" => i32::le,
+            "==" => i32::eq,
+            "!=" => i32::ne,
+            _ => unreachable!(),
+        };
+        if op(lhs, &instruction.cond_rhs) {
+            match instruction.op {
+                "inc" => *regs.entry(instruction.reg).or_insert(0) += instruction.diff,
+                "dec" => *regs.entry(instruction.reg).or_insert(0) -= instruction.diff,
+                _ => unreachable!(),
+            }
+            max_val = cmp::max(max_val, regs[instruction.reg]);
+        }
+    }
+    if part == 1 {
+        *regs.values().max().unwrap()
+    } else {
+        max_val
+    }
+}
+
+pub fn part1(input: &str) -> i32 {
+    part_(1, input)
+}
+
+pub fn part2(input: &str) -> i32 {
+    part_(2, input)
+}
+
+pub fn tests() {
+    let example = "b inc 5 if a > 1
+                   a inc 1 if b < 5
+                   c dec -10 if a >= 1
+                   c inc -20 if c == 10";
+    assert_eq!(part1(example), 1);
+    assert_eq!(part2(example), 10);
+}
