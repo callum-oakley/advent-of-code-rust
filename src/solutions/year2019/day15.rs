0349baac07b5731c0ca63df8d38c1ae52bf670b2
Callum Oakley <hello@callumoakley.net>
2023-11-22 11:42:23 +0000

    2019 15 search2

--- src/solutions/year2019/day15.rs
+++ src/solutions/year2019/day15.rs
@@ -1,7 +1,7 @@
 use crate::{
     grid::{Point, E, N, S, W, Z},
     intcode::VM,
-    search,
+    search2::{self, Queue},
 };
 
 #[derive(Clone)]
@@ -12,11 +12,9 @@ struct State {
     found_oxygen_system: bool,
 }
 
-impl search::State for State {
-    type HashKey = Point;
-
-    fn adjacent(&self) -> Box<dyn Iterator<Item = Self> + '_> {
-        Box::new([N, S, W, E].iter().enumerate().map(|(i, dir)| {
+impl State {
+    fn adjacent(&self) -> impl Iterator<Item = Self> + '_ {
+        [N, S, W, E].iter().enumerate().map(|(i, dir)| {
             let mut state = self.clone();
             state.vm.input(i64::try_from(i).unwrap() + 1);
             match state.vm.output() {
@@ -33,23 +31,29 @@ impl search::State for State {
                 _ => unreachable!(),
             }
             state
-        }))
-    }
-
-    fn hash_key(&self) -> Self::HashKey {
-        self.pos
+        })
     }
 }
 
 fn oxygen_system(input: &str) -> State {
-    search::breadth_first(State {
-        vm: VM::new(input),
-        pos: Z,
-        steps: 0,
-        found_oxygen_system: false,
-    })
-    .find(|state| state.found_oxygen_system)
-    .unwrap()
+    let mut q = search2::breadth_first(
+        State {
+            vm: VM::new(input),
+            pos: Z,
+            steps: 0,
+            found_oxygen_system: false,
+        },
+        |state| state.pos,
+    );
+    while let Some(state) = q.pop() {
+        if state.found_oxygen_system {
+            return state;
+        }
+        for s in state.adjacent() {
+            q.push(s);
+        }
+    }
+    unreachable!()
 }
 
 pub fn part1(input: &str) -> u32 {
@@ -57,12 +61,19 @@ pub fn part1(input: &str) -> u32 {
 }
 
 pub fn part2(input: &str) -> u32 {
-    let oxygen_system = oxygen_system(input);
-    search::breadth_first(State {
-        steps: 0,
-        ..oxygen_system
-    })
-    .map(|state| state.steps)
-    .max()
-    .unwrap()
+    let mut q = search2::breadth_first(
+        State {
+            steps: 0,
+            ..oxygen_system(input)
+        },
+        |state| state.pos,
+    );
+    let mut max_steps = 0;
+    while let Some(state) = q.pop() {
+        max_steps = max_steps.max(state.steps);
+        for s in state.adjacent() {
+            q.push(s);
+        }
+    }
+    max_steps
 }
